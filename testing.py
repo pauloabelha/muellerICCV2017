4c193850f00270f27a55b16e1cd214de17e72636
Paulo Ferreira <pauloabelha@gmail.com>
2018-05-04 20:03:25 +0100

    b

--- testing.py
+++ testing.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 root_folder = '/home/paulo/'
 filepath_prior = root_folder + 'output_HALNet_prior_1804790995.txt'
 filepath = root_folder + 'output_HALNet_1345115866.txt'
-filepath_jornet = root_folder + 'handtracking/' + 'output_JORNet_1392734821.txt'
+filepath_jornet = root_folder + 'handtracking/' + 'output_JORNet_1745897270.txt'
 
-mean_loss_prefix = 'Mean loss for last 10 iterations (average total loss): '
+mean_loss_prefix_jornet = 'Mean loss for last 100 iterations (average total loss): '
+mean_loss_prefix_halnet = 'Mean loss for last 10 iterations (average total loss): '
 prior_loss_prefix = 'Mean loss (prior) for last 10 iterations (average total loss): '
 iter_prefix = 'Training (Epoch #'
 
@@ -13,8 +14,8 @@ mean_losses = []
 ix = 0
 with open(filepath_jornet) as fp:
     for line in fp:
-        if mean_loss_prefix in line:
-            mean_loss = float(line[len(mean_loss_prefix):])
+        if mean_loss_prefix_jornet in line:
+            mean_loss = float(line[len(mean_loss_prefix_jornet):])
             mean_losses.append(mean_loss)
             print(mean_loss)
             ix += 1
@@ -26,20 +27,20 @@ plt.ylabel('Mean losses (joint pair dist)')
 plt.show()
 
 
-'''
+
 mean_losses_hm = []
 with open(filepath) as fp:
     for line in fp:
-        if mean_loss_prefix in line:
-            mean_loss_str = float(line[len(mean_loss_prefix):])
+        if mean_loss_prefix_halnet in line:
+            mean_loss_str = float(line[len(mean_loss_prefix_halnet):])
             mean_losses_hm.append(mean_loss_str / 21.0)
 
 mean_losses_hm_prior = []
 mean_losses_joint_dist_prior = []
 with open(filepath_prior) as fp:
     for line in fp:
-        if mean_loss_prefix in line:
-            mean_loss = float(line[len(mean_loss_prefix):])
+        if mean_loss_prefix_halnet in line:
+            mean_loss = float(line[len(mean_loss_prefix_halnet):])
         if prior_loss_prefix in line:
             prior_loss = float(line[len(prior_loss_prefix):])
             heatmap_loss = mean_loss - prior_loss
@@ -56,4 +57,3 @@ plt.show()
 plt.plot(mean_losses_joint_dist_prior)
 plt.ylabel('Mean losses (joint pair dist)')
 plt.show()
-'''
